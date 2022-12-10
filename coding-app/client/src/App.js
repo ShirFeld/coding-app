85e2ef8dabfba6cddcef0dcf3c851bb2e2c80f95
ShirFeld <shirfeldman1103@gmail.com>
2022-12-10 16:29:28 +0200

    update

--- coding-app/client/src/App.js
+++ coding-app/client/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import { BrowserRouter, Route, Routes, Navigate, HashRouter } from "react-router-dom";
-import { v4 as uuidV4 } from "uuid";
+
 
 
 // screens
 import Login from './screens/Login';
 import Lobby from './screens/Lobby'
-import ChatRoom from './screens/ChatRoom';
 import TextEditor from './components/TextEditor';
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/lobby" element={<Lobby />} />
-          <Route path="/chatRoom/" element={<ChatRoom />} />
           {/* <Route path="/documents" element={<Navigate replace to={`/documents/${uuidV4()}`} />}></Route> */}
           <Route path="/documents" element={<Navigate replace to={`/documents/:id`} />}></Route>
           <Route path="/documents/:id"
